6e2df0315d542973f60a78819ac573fec4ae5637
mohamed gassama <bouyagassama@gmail.cpm>
2017-06-18 03:01:03 -0400

    progess

---
+++ src/Compoment/Greeting/Greeting.jsx
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+const Greeting = (props) => {
+    return (
+        <div>
+        	<h1>hello</h1>
+        </div>
+    );
+};
+
+
+
+export default Greeting;
